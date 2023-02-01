a0e70d7d06b44e7f8f088957f2367cfd2b100009
Giagnacovo Ivan <93552640+giagnai@users.noreply.github.com>
2023-02-01 18:35:51 -0300

    Add files via upload

---
+++ ejercicios_json/script.js
@@ -0,0 +1,10 @@
+//OBJETO VACIO-JSON VACIO
+var JSONObj = {};
+
+//NUEVO OBJETO
+var JSONObj = new Object();
+
+//OBJETO CON ATRIBUTOS
+var JSONObj = {"nombrelibro":"El hacedor", "precio":500};
+
+console.log(JSONObj);
